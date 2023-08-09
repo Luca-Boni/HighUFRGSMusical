e93a8e71d320f83492b9005661b6d5f44f8e312b
TakatJJ <galeanodomingues@gmail.com>
2023-08-08 23:54:29 -0300

    Create Guitarra.
    Co-authored-by: pedrohbroewer <pedrohbroewer@users.noreply.github.com>
    Co-authored-by: Luca Boni <Luca-Boni@users.noreply.github.com>

---
+++ src/implementacao/BotoesInstrumentos.java
@@ -0,0 +1,13 @@
+package implementacao;
+import java.util.HashMap;
+
+public class BotoesInstrumentos {
+    private HashMap<String,BotaoInstrumento> layoutBotoes;
+    // private String caminhoImgBaixo;
+    // private String caminhoImgGuitarra;
+    // private String caminhoImgBateria;
+    // private String caminhoImgPiano;
+    
+    
+
+}
