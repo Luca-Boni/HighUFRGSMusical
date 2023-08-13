0174fa2ded723c16d5d66eadb7b274b439180839
Cauê Scotti <caue.scotti@gmail.com>
2023-08-13 15:15:13 -0300

    Inserção arquivos de áudio e slider de volume

--- src/interfaceGrafica/SliderVolume.java
+++ src/interfaceGrafica/SliderVolume.java
@@ -2,68 +2,78 @@ package interfaceGrafica;
 
 import java.util.ArrayList;
 import implementacao.Tecla;
+import programaPrincipal.ProgramaPrincipal;
 
 import com.raylib.Jaylib;
 import com.raylib.Jaylib.Vector2;
 
 public class SliderVolume {  //provavelmente vai ser completamente reformulada, apenas esboço.
 
-    private Jaylib.Texture2D textura_Claro;
+    private Jaylib.Texture2D barraClaro;
+    private Jaylib.Texture2D barraEscuro;
+    private Jaylib.Texture2D bolaClaro;
+    private Jaylib.Texture2D bolaEscuro;
+
+    private Vector2 posicaoBola;
+    private final Vector2 POSICAO_BARRA = new Jaylib.Vector2(4400*ProgramaPrincipal.ESCALA, 80*ProgramaPrincipal.ESCALA); //y é 1280 ou 80
 
     private final int VOLUME_UP = 265;
-    
     private final int VOLUME_DOWN = 264;
- 
-    private Jaylib.Texture2D textura_Escuro;
-
-    private Vector2 posicao;
-
-    private int angulo;
 
     private float volumeAtual;
-
-    private final int volumeMaximo = 100;
-
-    private final int volumeMinimo = 0;
+    private final float volumeMaximo = 1;
+    private final float volumeMinimo = 0;
 
     public SliderVolume(){
-        this.posicao = new Vector2(0,90);
-        this.angulo = 0;
+        this.posicaoBola = new Vector2(4390*ProgramaPrincipal.ESCALA, 585*ProgramaPrincipal.ESCALA);
         this.volumeAtual = 0.5f;
-        this.textura_Claro = Jaylib.LoadTexture("./assets/ui/modo_claro/slider_volume/knob_e_indicador.png");
-        this.textura_Escuro = Jaylib.LoadTexture("./assets/ui/modo_escuro/slider_volume/knob_e_indicador.png");
+        this.barraClaro = Jaylib.LoadTexture("./assets/ui/modo_claro/slider_volume/barra_volume.png");
+        this.bolaClaro = Jaylib.LoadTexture("./assets/ui/modo_claro/slider_volume/bola_claro.png");
+        this.barraEscuro = Jaylib.LoadTexture("./assets/ui/modo_escuro/slider_volume/barra_volume.png");
+        this.bolaEscuro = Jaylib.LoadTexture("./assets/ui/modo_escuro/slider_volume/bola_escuro.png");
     }
 
     
-    private Jaylib.Texture2D retrieveTexture(boolean modoEscuro){
+    /*private Jaylib.Texture2D retrieveTexture(boolean modoEscuro){
         return modoEscuro? textura_Escuro : textura_Claro;
-    } 
+    } */
     
     
 
 
-    private void desenhar (boolean modoEscuro){
-        Jaylib.DrawTextureEx(retrieveTexture(modoEscuro), posicao, angulo, 0.25f , Jaylib.WHITE);
+    private void desenhar (boolean modoEscuro){ //Tenho que mudar isso daqui
+        if (modoEscuro){
+            Jaylib.DrawTextureEx(barraEscuro, POSICAO_BARRA, 0, ProgramaPrincipal.ESCALA , Jaylib.WHITE);
+            Jaylib.DrawTextureEx(bolaEscuro, posicaoBola, 0, ProgramaPrincipal.ESCALA, Jaylib.WHITE);
+        }
+        else{
+            Jaylib.DrawTextureEx(barraClaro, POSICAO_BARRA, 0, ProgramaPrincipal.ESCALA , Jaylib.WHITE);
+            Jaylib.DrawTextureEx(bolaClaro, posicaoBola, 0, ProgramaPrincipal.ESCALA, Jaylib.WHITE);
+        }
     }
 
     
     public void criaBotaoVolume(boolean modoEscuro,ArrayList<Tecla> teclas) {
         for (Tecla tecla : teclas) {
             if (tecla.equals(new Tecla(VOLUME_UP,false))) {
-                volumeAtual += 0.1f;
-                angulo += 10;
+                volumeAtual += 0.01f;
                 
                 if (volumeAtual > volumeMaximo) {
                     volumeAtual = volumeMaximo;
                 }
+                else{
+                    posicaoBola.y(posicaoBola.y() - 11*ProgramaPrincipal.ESCALA);
+                }
                 Jaylib.SetMasterVolume(volumeAtual);
             }
             else if (tecla.equals(new Tecla(VOLUME_DOWN,false))){
-                volumeAtual -= 0.1f;
-                angulo -= 10;
+                volumeAtual -= 0.01f;
                 
                 if (volumeAtual < volumeMinimo) {
                     volumeAtual = volumeMinimo;
+                }
+                else{
+                    posicaoBola.y(posicaoBola.y() + 11*ProgramaPrincipal.ESCALA);
                 }
                 Jaylib.SetMasterVolume(volumeAtual);
             }
